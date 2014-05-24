7f8538cb882dbcd6886ba2cb05f53e506c499a7d
Matthew Purdy <mpurdy1973work@gmail.com>
2014-05-24 11:11:03 -0400

    Update stop-hadoop.bash

--- cdh4-accumulo/scripts/stop-hadoop.bash
+++ cdh4-accumulo/scripts/stop-hadoop.bash
@@ -4,8 +4,8 @@ sleep_time=${1:-15}
 script_path=$(dirname $(readlink -f $0))
 source $script_path/base-run.bash
 
-stop_service hadoop-0.20-mapreduce-tasktracker $start_time
-stop_service hadoop-0.20-mapreduce-jobtracker $start_time
-stop_service hadoop-hdfs-datanode $start_time
-stop_service hadoop-hdfs-secondarynamenode $start_time
-stop_service hadoop-hdfs-namenode $start_time 
+stop_service hadoop-0.20-mapreduce-tasktracker $sleep_time
+stop_service hadoop-0.20-mapreduce-jobtracker $sleep_time
+stop_service hadoop-hdfs-datanode $sleep_time
+stop_service hadoop-hdfs-secondarynamenode $sleep_time
+stop_service hadoop-hdfs-namenode $sleep_time 
