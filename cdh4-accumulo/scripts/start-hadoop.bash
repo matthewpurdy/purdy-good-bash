6207f116edbb42076b124404d2c20dc03c08a2d3
Matthew Purdy <mpurdy1973work@gmail.com>
2014-05-23 22:49:09 -0400

    Update start-hadoop.bash

--- cdh4-accumulo/scripts/start-hadoop.bash
+++ cdh4-accumulo/scripts/start-hadoop.bash
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-function start_service {
-	echo -n "### starting $1 ..."
-	service $1 start
-	echo " $1 started"
-	echo -n "sleeping for $2 seconds..."
-	sleep $2
-	echo " waking up"
-	echo $(service $1 status)
-}
+sleep_time=${1:-15}
+script_path=$(dirname $(readlink -f $0))
+source $script_path/base-run.bash
 
-start_service hadoop-hdfs-namenode 15
-start_service hadoop-hdfs-secondarynamenode 15
-start_service hadoop-hdfs-datanode 15
-start_service hadoop-0.20-mapreduce-jobtracker 15
-start_service hadoop-0.20-mapreduce-tasktracker 15
-
+start_service hadoop-hdfs-namenode $start_time 
+start_service hadoop-hdfs-secondarynamenode $start_time
+start_service hadoop-hdfs-datanode $start_time
+start_service hadoop-0.20-mapreduce-jobtracker $start_time
+start_service hadoop-0.20-mapreduce-tasktracker $start_time
