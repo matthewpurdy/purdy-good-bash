cbe698ef4333b3d336c2d2170a69cbaf9f252cce
Matthew Purdy <mpurdy1973work@gmail.com>
2014-05-23 22:47:33 -0400

    Update start-zookeeper.bash

--- cdh4-accumulo/scripts/start-zookeeper.bash
+++ cdh4-accumulo/scripts/start-zookeeper.bash
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-function start_service {
-	echo -n "### starting $1 ..."
-	service $1 start
-	echo " $1 started"
-	echo -n "sleeping for $2 seconds..."
-	sleep $2
-	echo " waking up"
-	echo $(service $1 status)
-}
+sleep_time=${1:-15}
+script_path=$(dirname $(readlink -f $0))
+source $script_path/base-run.bash
 
-start_service zookeeper-server 15
-
+start_service zookeeper-server $sleep_time
