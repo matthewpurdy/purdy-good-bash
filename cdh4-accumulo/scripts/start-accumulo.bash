11c2e16ad2c13e978cc22762105b725792ae955e
Matthew Purdy <mpurdy1973work@gmail.com>
2014-05-23 22:45:45 -0400

    Update start-accumulo.bash

--- cdh4-accumulo/scripts/start-accumulo.bash
+++ cdh4-accumulo/scripts/start-accumulo.bash
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-function start_service {
-	echo -n "### starting $1 ..."
-	service $1 start
-	echo " $1 started"
-	echo -n "sleeping for $2 seconds..."
-	sleep $2
-	echo " waking up"
-	echo $(service $1 status)
-}
+sleep_time=${1:-15}
+script_path=$(dirname $(readlink -f $0))
+source $script_path/base-run.bash
 
-start_service accumulo-master 15
-start_service accumulo-monitor 15
-start_service accumulo-gc 15
-start_service accumulo-tracer 15
-start_service accumulo-tserver 15
-start_service accumulo-logger 15
-
+start_service accumulo-master $sleep_time 
+start_service accumulo-monitor $sleep_time
+start_service accumulo-gc $sleep_time
+start_service accumulo-tracer $sleep_time
+start_service accumulo-tserver $sleep_time
+start_service accumulo-logger $sleep_time
