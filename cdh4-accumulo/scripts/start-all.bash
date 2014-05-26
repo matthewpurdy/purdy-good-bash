5c8f64b210861db219a7f52785944c88cfc54c96
Matthew Purdy <mpurdy1973work@gmail.com>
2014-05-26 12:02:57 -0400

    Update start-all.bash

--- cdh4-accumulo/scripts/start-all.bash
+++ cdh4-accumulo/scripts/start-all.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./start-zookeeper.bash $1
-./start-hadoop.bash $1
-./start-accumulo.bash $1
+script_path=$(dirname $(readlink -f $0))
+
+$script_path/start-zookeeper.bash $1
+$script_path/start-hadoop.bash $1
+$script_path/start-accumulo.bash $1
 
